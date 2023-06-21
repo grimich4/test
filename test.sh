a56c504fcda38ea10c501862f7a9fecb201872f8
grimich4 <gaspard.david@hotmail.fr>
2023-06-21 11:30:57 +0200

    changes

---
+++ test.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+touch find.out myfind.out
+
+check()
+{
+    printf "${BLUE}________________________NEW_TEST________________________${NC}\n"
+    echo test\: "$@"
+
+    which $@ > find.out
+    find_return_code=$?
+
+    ./which2.sh $@ > myfind.out
+    myfind_return_code=$?
+
+    echo -n "exit_code: "
+    [ $find_return_code -eq $myfind_return_code ] && printf "$GREEN[OK]$NC\n" || printf "$RED[KO]$NC\n"
+
+    diff -u find.out myfind.out
+    output_diff=$?
+    echo -n "stdout: "
+    [ $output_diff -eq 0 ] && printf "$GREEN[OK]$NC\n" || printf "$RED[KO]$NC\n"
+}
+
+check gcc
+check -a gcc
+check -d gcc
+check gcc curl which
+check -a gcc curl which
+check -d gcc curl which
+check gcc curl which dontexist
+check -a gcc curl which dontexist
+check -d gcc curl which dontexist
+check gcc curl -a which
+check -a gcc curl -a which
+check -d gcc curl -a which
+check dontexist
+
+rm find.out myfind.out
--- which2.sh
+++ which2.sh
@@ -10,11 +10,16 @@ if [ $# -eq 0 ]; then
 fi
 
 print_all_paths=0
+return_code=0
+has_found_once=0
 
 # Check if the first argument is the -a option
 if [ "$1" = "-a" ]; then
   print_all_paths=1
   shift  # Remove the -a option from the arguments
+elif [ "${1#-}" != "$1" ]; then
+  echo >&2 "which: unknown option '$1'"
+  exit 1
 fi
 
 # Loop through each command provided as an argument
@@ -27,6 +32,7 @@ for command in "$@"; do
     if [ -x "$command" ]; then
       echo "$command"
       found=1
+      has_found_once=1
       if [ "$print_all_paths" -eq 0 ]; then
         break  # Stop searching if -a option is not provided
       fi
@@ -41,6 +47,7 @@ for command in "$@"; do
       if [ -x "$full_path" ]; then
         echo "$full_path"
         found=1
+        has_found_once=1
         if [ "$print_all_paths" -eq 0 ]; then
           break  # Stop searching if -a option is not provided
         fi
@@ -50,6 +57,16 @@ for command in "$@"; do
 
   # If the command is not found, display an appropriate message
   if [ "$found" -eq 0 ]; then
+    if [ "$return_code" -eq 0 ]; then
+        return_code=2
+    fi
     echo "which: $command: Command not found."
   fi
 done
+
+if [ "$return_code" -eq 2 ]; then
+  if [ "$has_found_once" -eq 1 ]; then     
+      return_code=1
+  fi
+fi
+return $return_code
